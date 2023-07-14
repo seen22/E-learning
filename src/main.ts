2448a012e835b9b3cc7a4f8829450afc423037d8
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2023-07-14 15:39:46 +0200

    fixed both functions

--- src/main.ts
+++ src/main.ts
@@ -20,40 +20,28 @@ const reflectionQuestions = [
     "Wie könnten Bildungsinstitutionen und Regierungen die Integration von OER in Bildungssysteme fördern und unterstützen?"
 ];
 
+
+const codeOfConduct = ['Anleitung für den Raum',
+    'Schaut euch zuerst im Inneren um. Wenn ihr ins freie tretet findet ihr neben der Tür eine Einleitung und rechts unten eine Zusammenfassung.' +
+    ' Weitere Elemente müsst ihr entdecken.', 'Es gibt einen Bereich, wo ihr eine zufällig ausgewählte Frage bekommt,' +
+    ' die euch zur Reflektion dienen soll, bzw. wenn ihr mit mehreren den Jitsi-Raum im linken Außenbereich betretet, gemeinsam bearbeitet werden kann.','Der Jitsi Raum im unteren Außenbereich soll genutzt werden für Reflektionsgespräche über die Lernerfahrung, wenn man mehrere User auf der Map ist.\n',
+    ,'Verhaltensregeln / Code of Conduct\n',
+    '\n' +
+    'Für die Etablierung einer produktiven Lernumgebung ist es uns wichtig, dass sich jeder Besucher in unserem Work Adventure-Raum wohl fühlt.',
+    'Wir bitten euch um stets respektvollen und konstruktiven Umgang, wenn ihr euch hier begegnet. Die folgenden Regeln gelten uneingeschränkt für diesen Raum:\n',
+    'Alle Menschen sind gleich und wir tolerieren keinen Rassismus, Sexismus, Klassismus, Antisemitismus oder andere Formen von Gruppen-bezogener Menschenfeindlichkeit.\n',
+    'Wir heißen jeden Menschen in seiner Individualität willkommen, und tolerieren keine Beleidigungen, persönliche Anfeindungen, trans-feindliche Äußerungen wie z.B. Deadnaming.\n',
+    'Wir begrüßen kritische Auseinandersetzungen um die Sache, wir tolerieren kein unsachliches Abgleiten von Debatten und bemühen uns solche Unterhaltungen auf den Kern zurückzuführen.\n',
+    'Wir wollen, dass das Lernen positiv erfahren wird und möchten daher, dass ihr diesen Raum nur freiwillig in aufgeschlossener Stimmung erkundet und nicht zB weil ihr euch im Rahmen eines Seminars genötigt fühlt.\n',
+    'Wir wünschen eine angenehme Lernerfahrung.\n'];
+
 // Zufällige Frage auswählen
 function getRandomQuestion() {
     const randomIndex = Math.floor(Math.random() * reflectionQuestions.length);
     return reflectionQuestions[randomIndex];
 }
 
-// Popup anzeigen
-function showRandomQuestion() {
-    const question = getRandomQuestion();
-    currentPopup = WA.ui.openPopup("reflectionPopup", question, []);
-}
 
-function showCodeOfConductPopup() {
-    const popupContent = `
-    <div>
-      <h2>Anleitung für den Raum</h2>
-      <p>Schaut euch zuerst im inneren um, wenn ihr ins freie tretet findet ihr neben der Tür eine Einleitung und rechts unten eine Zusammenfassung. Weitere Elemente müsst ihr entdecken. Es gibt einen Bereich, wo ihr eine zufällig ausgewählte Frage bekommt, die euch zur Reflektion dienen soll, bzw. wenn ihr mit mehreren den Jitsi-Raum im linken Außenbereich betretet, gemeinsam bearbeitet werden kann. Der Jitsi Raum im unteren Außenbereich soll genutzt werden für Reflektionsgespräche über die Lernerfahrung, wenn man mehrere User auf der Map ist.</p>
-    
-      <h2>Verhaltensregeln / Code of Conduct</h2>
-      <p>Für die Etablierung einer produktiven Lernumgebung ist es uns wichtig, dass sich jeder Besucher in unserem Work Adventure-Raum wohl fühlt. Wir bitten euch um stets respektvollen und konstruktiven Umgang, wenn ihr euch hier begegnet. Die folgenden Regeln gelten uneingeschränkt für diesen Raum:</p>
-      
-      <ul>
-        <li>Alle Menschen sind gleich und wir tolerieren keinen Rassismus, Sexismus, Klassismus, Antisemitismus oder andere Formen von Gruppen-bezogener Menschenfeindlichkeit.</li>
-        <li>Wir heißen jeden Menschen in seiner Individualität willkommen und tolerieren keine Beleidigungen, persönliche Anfeindungen, trans-feindliche Äußerungen wie z.B. Deadnaming.</li>
-        <li>Wir begrüßen kritische Auseinandersetzungen um die Sache, wir tolerieren kein unsachliches Abgleiten von Debatten und bemühen uns solche Unterhaltungen auf den Kern zurückzuführen.</li>
-        <li>Wir wollen, dass das Lernen positiv erfahren wird und möchten daher, dass ihr diesen Raum nur freiwillig in aufgeschlossener Stimmung erkundet und nicht zB weil ihr euch im Rahmen eines Seminars genötigt fühlt.</li>
-      </ul>
-      
-      <p>Wir wünschen eine angenehme Lernerfahrung.</p>
-    </div>
-  `;
-
-    currentPopup = WA.ui.openPopup("codeOfConductPopup", popupContent, []);
-}
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -61,17 +49,22 @@ WA.onInit().then(() => {
     console.log('Player tags: ',WA.player.tags)
 
     WA.room.area.onEnter('Reflektionsfragen').subscribe(() => {
-        showRandomQuestion();
+        WA.chat.open();
+        WA.chat.sendChatMessage(getRandomQuestion(), 'Sarah und Ingmar');
     });
 
-    WA.room.area.onLeave('Reflektionsfragen').subscribe(closePopup);
+    WA.room.area.onLeave('Reflektionsfragen').subscribe(() => WA.chat.close());
 
 
     WA.room.area.onEnter('codeOfConduct').subscribe(() => {
-        showCodeOfConductPopup();
+        WA.chat.open();
+        codeOfConduct.forEach((message) => {
+            if(message != null) WA.chat.sendChatMessage(message, 'Sarah und Ingmar');
+        })
+
     });
 
-    WA.room.area.onLeave('codeOfConduct').subscribe(closePopup);
+    WA.room.area.onLeave('codeOfConduct').subscribe(() => WA.chat.close());
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
