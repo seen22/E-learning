eec6eb126d4cb5543c2270eea8114b276937c87e
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2023-07-22 16:52:36 +0200

    changed popUp into html Pages and added Text for Intros

--- src/main.ts
+++ src/main.ts
@@ -17,18 +17,6 @@ const reflectionQuestions = [
     "Wie könnten Bildungsinstitutionen und Regierungen die Integration von OER in Bildungssysteme fördern und unterstützen?"
 ];
 
-const codeOfConduct = ['Anleitung für den Raum',
-    'Schaut euch zuerst im Inneren um. Wenn ihr ins freie tretet findet ihr neben der Tür eine Einleitung und rechts unten eine Zusammenfassung. ' +
-    'Weitere Elemente müsst ihr entdecken. Es gibt einen Bereich, wo ihr eine zufällig ausgewählte Frage bekommt, die euch zur Reflektion dienen soll, ' +
-    'bzw. wenn ihr mit mehreren den Jitsi-Raum im linken Außenbereich betretet, gemeinsam bearbeitet werden kann. ' +
-    'Der Jitsi Raum im unteren Außenbereich soll genutzt werden für Reflektionsgespräche über die Lernerfahrung, wenn man mehrere User auf der Map ist.\n',
-    'Verhaltensregeln / Code of Conduct',
-    'Für die Etablierung einer produktiven Lernumgebung ist es uns wichtig, dass sich jeder Besucher in unserem Work Adventure-Raum wohl fühlt. Wir bitten euch um stets respektvollen und konstruktiven Umgang, wenn ihr euch hier begegnet. Die folgenden Regeln gelten uneingeschränkt für diesen Raum:\n',
-    '1. Alle Menschen sind gleich und wir tolerieren keinen Rassismus, Sexismus, Klassismus, Antisemitismus oder andere Formen von Gruppen-bezogener Menschenfeindlichkeit.',
-    '2.Wir heißen jeden Menschen in seiner Individualität willkommen, und tolerieren keine Beleidigungen, persönliche Anfeindungen, trans-feindliche Äußerungen wie z.B. Deadnaming.',
-    '3. Wir begrüßen kritische Auseinandersetzungen um die Sache, wir tolerieren kein unsachliches Abgleiten von Debatten und bemühen uns solche Unterhaltungen auf den Kern zurückzuführen.',
-    '4. Wir wollen, dass das Lernen positiv erfahren wird und möchten daher, dass ihr diesen Raum nur freiwillig in aufgeschlossener Stimmung erkundet und nicht zB weil ihr euch im Rahmen eines Seminars genötigt fühlt.',
-    '\nWir wünschen eine angenehme Lernerfahrung.'];
 
 let currentPopup: any = undefined;
 
@@ -41,7 +29,6 @@ function getRandomQuestion() {
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags)
-    let message: string = '';
 
     WA.room.area.onEnter('Reflektionsfragen').subscribe(() => {
         currentPopup = WA.ui.openPopup("reflectionPopUp", getRandomQuestion(), [{
@@ -52,27 +39,10 @@ WA.onInit().then(() => {
     });
 
     WA.room.area.onLeave('Reflektionsfragen').subscribe(() => {
-        message = '';
         closePopup();
     });
 
 
-
-    WA.room.area.onEnter('codeOfConduct').subscribe(() => {
-        codeOfConduct.forEach((string) => {
-            message += string + '\n';
-        })
-        currentPopup = WA.ui.openPopup("codeOfConductPopUp", message, [{
-            label: 'Close', // Button label
-            className: 'primary', // Button class (primary, secondary, etc.)
-            callback: closePopup // Button callback function
-        }]);
-    });
-
-    WA.room.area.onLeave('codeOfConduct').subscribe(() => {
-        message = '';
-        closePopup();
-    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
